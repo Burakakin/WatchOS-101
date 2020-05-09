4ebb92bb9976ee8b663043d2e7bf5fcb8aff340a
Burak Akın <akinburak5@gmail.com>
2020-05-09 23:59:52 +0300

    Initial Commit

---
+++ WatchOS-101/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatchOS-101
+//
+//  Created by Burak Akin on 9.05.2020.
+//  Copyright © 2020 Burak Akin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
