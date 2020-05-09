4b411461619cefee0aaa2cc10c86184deba1dbbf
Burak Akın <akinburak5@gmail.com>
2020-05-10 00:01:54 +0300

    WatchOS target added and works independently.

---
+++ WatchKit-101 Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchKit-101 Extension
+//
+//  Created by Burak Akin on 10.05.2020.
+//  Copyright © 2020 Burak Akin. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
