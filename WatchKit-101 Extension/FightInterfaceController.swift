f5c6284ab8d2b19b27ac9f8333edd074034948ec
Burak Akın <akinburak5@gmail.com>
2020-05-10 03:03:11 +0300

    Part 1 -  Share Model between iOS and WatchOS, Add a watchOS interface controller to the Storyboard, and lay out the complex interface objects using nested groups.

---
+++ WatchKit-101 Extension/FightInterfaceController.swift
@@ -0,0 +1,56 @@
+//
+//  FightInterfaceController.swift
+//  WatchKit-101 Extension
+//
+//  Created by Burak Akin on 10.05.2020.
+//  Copyright © 2020 Burak Akin. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+class FightInterfaceController: WKInterfaceController {
+    
+    @IBOutlet var flightLabel: WKInterfaceLabel!
+    @IBOutlet var routeLabel: WKInterfaceLabel!
+    @IBOutlet var boardingLabel: WKInterfaceLabel!
+    @IBOutlet var boardTimeLabel: WKInterfaceLabel!
+    @IBOutlet var statusLabel: WKInterfaceLabel!
+    @IBOutlet var gateLabel: WKInterfaceLabel!
+    @IBOutlet var seatLabel: WKInterfaceLabel!
+    
+    var flight: Flight? {
+        didSet {
+            
+            if let flight = flight {
+                flightLabel.setText("Flight \(flight.shortNumber)")
+                routeLabel.setText(flight.route)
+                boardingLabel.setText("\(flight.number) Boards")
+                boardTimeLabel.setText(flight.boardsAt)
+                
+                if flight.onSchedule {
+                    statusLabel.setText("On Time")
+                }
+                else {
+                    statusLabel.setText("Delayed")
+                    statusLabel.setTextColor(.red)
+                }
+                
+                gateLabel.setText("Gate \(flight.gate)")
+                seatLabel.setText("Seat \(flight.seat)")
+                
+                
+                
+            }
+            
+        }
+    }
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        flight = Flight.allFlights().first
+        // Configure interface objects here.
+    }
+    
+}
